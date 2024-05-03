12daa3959ba3f61e7870bed26e7c968a37f3547a
Sofia <sofiarodrigues45@gmail.com>
2024-05-02 21:26:19 -0300

    teste

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import View.TelaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaPrincipal objetotela = new TelaPrincipal();
+        objetotela.setVisible(true);
+    }
+}
